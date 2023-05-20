6a196026137e2537ac3ae94150c983abf4fd7f61
Md Jahid Al Hassan <jahidcse05@gmail.com>
2023-05-20 18:03:34 +0600

    Api end Point Update

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const app = express()
-const port = process.env.PROT || 5000;
-
-
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 2500;
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.o7ebdmy.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,31 +18,47 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const allToysCollection = client.db("ToysCollection").collection("allToys");
+
+   
+
+    app.get("/collection", async (req, res) => {
+      try {
+        const coursor = {}
+        const data =  allToysCollection.find(coursor);
+        const result = await data.toArray();
+        res.send(result);
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-
-app.get('/', (req, res) => {
-    res.send("HELLO KIDS");
-})
+app.get("/", (req, res) => {
+  res.send("HELLO KIDS");
+});
 
 app.listen(port, () => {
-    console.log(`Kids land server is running: ${port}`);
-})
+  console.log(`Kids land server is running: ${port}`);
+});
